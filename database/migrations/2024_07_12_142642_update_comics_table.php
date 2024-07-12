a1ec1b09160a5d35d35f37d79628e1b849329a47
DanieleBernassola <danielebernassola02@gmail.com>
2024-07-12 17:21:52 +0200

    [UPD] Created migration and resource page

---
+++ database/migrations/2024_07_12_142642_update_comics_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('comics', function (Blueprint $table) {
+            $table->string('title', 100);
+            $table->text('description');
+            $table->text('thumb', 100);
+            $table->decimal('price', 3, 2);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 50);
+            $table->json('artists');
+            $table->json('writers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('comics', function (Blueprint $table) {
+            $table->dropColumn('title');
+            $table->dropColumn('description');
+            $table->dropColumn('thumb');
+            $table->dropColumn('price');
+            $table->dropColumn('series');
+            $table->dropColumn('sale_date');
+            $table->dropColumn('type');
+            $table->dropColumn('artists');
+            $table->dropColumn('writers');
+        });
+    }
+};
