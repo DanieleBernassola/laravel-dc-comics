d8a06b7da57fdbae2e84a3d494e0df410ef0c0a5
DanieleBernassola <danielebernassola02@gmail.com>
2024-07-12 15:47:10 +0200

    [UPD] Created model and resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\ComicController;
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ROTTA HOMEPAGE
+Route::get('/', [PageController::class, 'index']);
+
+// ELENCO FUMETTI
+// Route::get('/comics', [ComicController::class, 'index']);
+
+Route::resource('comics', ComicController::class);
